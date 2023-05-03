adc8578d801afd703c4ff6450742896bca4f1dab
The_Yatagarasu <Hugo@mail.com>
2023-05-03 11:11:48 +0200

    Añadir la cabecera

--- src/main/java/Comprobador.java
+++ src/main/java/Comprobador.java
@@ -1,4 +1,8 @@
-import java.util.*;
+/*
+ * -> Funcionalidad: comprobar que la cadena de carácteres contenga 5 carácteres en mayúsculas. <-
+ * -> Autor: Hugo Jiménez (The_Yatagarasu) <-
+ * -> Fecha: 03/05/2023 <-
+ */
 public class Comprobador {
     private static boolean Comprueba(String palabra, Integer longitud){
         if(palabra.length()==longitud && palabra.equals(palabra.toUpperCase())){
--- src/test/java/GeneradorTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-class GeneradorTest {
-
-    @org.junit.jupiter.api.Test
-    void generar_pista() {
-
-    }
-}
