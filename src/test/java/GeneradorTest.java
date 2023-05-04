55dd25690128cfce96d60314ec589870b576c1b7
Diego Lacasa Montes <200066@iessanalberto.com>
2023-05-04 11:03:02 +0200

    Commit Test Generador - Diego

--- src/test/java/GeneradorTest.java
+++ src/test/java/GeneradorTest.java
@@ -4,6 +4,17 @@ class GeneradorTest {
 
     @org.junit.jupiter.api.Test
     void generar_pista() {
+        Generador generador = new Generador();
+        String comparacion1 = generador.generar_pista("DIEGO", "JUEGO");
+        assertEquals("--OOO", comparacion1, "Resultado no esperado");
 
+        String comparacion2 = generador.generar_pista("DIEGO", "MOVIL");
+        assertEquals("-X-X-", comparacion2, "Resultado no esperado");
+
+        String comparacion3 = generador.generar_pista("DIEGO", "TIRON");
+        assertEquals("-O-X-", comparacion3, "Resultado no esperado");
+
+        String comparacion4 = generador.generar_pista("DIEGO", "DIEGO");
+        assertEquals("OOOOO", comparacion4, "Resultado no esperado");
     }
 }
