efd4410305fe377b7ea7d2844528fe4b11ec5cd6
The_Yatagarasu <Hugo@mail.com>
2023-05-10 11:08:16 +0200

    Creación de la clase de los récords.

---
+++ src/main/java/Records.java
@@ -0,0 +1,19 @@
+public class Records{
+    /*
+     * -> Hecho por Hugo Jiménez (The_Yatagarasu) <-
+     * -> Hecho el 10-05-2023 <-
+     */
+    public static String recordDeIntentos() {
+        String top1="Mauro";
+        String top2="Nacho";
+        String top3="Ramiro";
+        String top4="Augusto";
+        String top5="Fernando";
+        String top6="Facundo";
+        String top7="Carlos";
+        String top8="Víctor";
+        String top9="Jorge";
+        String top10="Matías";
+        return "Los 10 mejores resultados son: \n"+top1+" -> con 1 intento/s\n"+top2+"-> con 2 intento/s\n"+top3+"-> con 4 intento/s\n"+top4+"-> con 10 intento/s\n"+top5+"-> con 19 intento/s\n"+top6+"-> con 25 intento/s\n"+top7+"-> con 31 intento/s\n"+top8+"-> con 39 intento/s\n"+top9+"-> con 43 intento/s\n"+top10;
+    }
+}
